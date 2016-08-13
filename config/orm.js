43a544e47c0131da06df36db2ce658896885cbd0
davidb9790 <davidbermudez97@gmail.com>
2016-08-12 22:47:59 -0400

    updates

--- config/orm.js
+++ config/orm.js
@@ -34,29 +34,5 @@ var orm = {
 }//last one 
 
 
-// orm.selectAll() = function(){
-
-// }
-
-
-
-// var connection=require('./connection.js');
-
-// var orm={
-//     selectAll: function(tableInput,cb){
-//         var s='SELECT * FROM '+tableInput+';';
-//         connection.query(s,function(err,result){
-//             cb(result);})},
-
-//     insertData: function(tableInput,name,cb){ 
-//         var s='INSERT INTO '+tableInput+' (burger_name) VALUES ("'+ name +'")';
-//         connection.query(s,function(err,result){
-//             cb(result);})},
-
-//     changeData: function(tableInput,id,cb){
-//         var s='UPDATE '+tableInput+' SET devoured=1 WHERE id='+id+';';
-//         connection.query(s,function(err,result){
-//             console.log(result);
-//             cb(result);})}};
     
 module.exports=orm;
